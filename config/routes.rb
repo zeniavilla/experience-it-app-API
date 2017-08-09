e8aae37bdf4c54cd7234ad1c35fd3cd31e9d9d1f
Zenia <zeniavilla@hotmail.com>
2017-08-09 13:39:53 -0700

    Add /api/experiences, /api/sign_up, /api/profile/:id routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :experiences, except: [:destroy, :edit, :update]
+
+    post '/sign_up', to: 'register#create', as: 'sign_up'
+    get '/profile/:id', to: 'profile#show', as: 'profile'
+  end
 end
