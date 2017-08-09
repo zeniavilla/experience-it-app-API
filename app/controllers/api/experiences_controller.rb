b11e82f2ed6b2c95b770e8fb6d8421b65d387348
Zenia <zeniavilla@hotmail.com>
2017-08-09 13:53:46 -0700

    Add actions for Experiences Controller

---
+++ app/controllers/api/experiences_controller.rb
@@ -0,0 +1,26 @@
+class Api::ExperiencesController < ApplicationController
+
+    def index
+        render json: Experience.all
+    end
+
+    def create
+        experience = Experience.new(experience_params)
+        if experience.save
+            render json: experience
+        else
+            render json: { message: experience.errors }, status: 400
+        end
+    end
+
+    def show
+        render json: Experience.find_by(id: params[:id])
+    end
+
+    private
+
+    def experience_params
+        params.require(:experience).permit(:name, :category, :location, :time, :recommended_times, :img_url)
+    end
+
+end
