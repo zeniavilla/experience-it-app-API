a1dc2a703289e903c547eebf9bb8adbcd89ae209
Zenia <zeniavilla@hotmail.com>
2017-08-09 13:57:41 -0700

    add Register Controller with create action

---
+++ app/controllers/api/register_controller.rb
@@ -0,0 +1,18 @@
+class Api::RegisterController < ApplicationController
+
+    def create
+        user = User.new(user_params)
+        if user.save
+            render json: user
+        else
+            render json: { messages: user.errors }, status: 400
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password)
+    end
+
+end
