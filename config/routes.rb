e7a1dc3a25251e1d695e082978a57dd1690eb55c
Zenia <zeniavilla@hotmail.com>
2017-08-15 09:18:58 -0700

    Add update route to back-end

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :experiences, except: [:destroy, :edit, :update]
+    resources :experiences, except: [:destroy, :edit]
+  end
 
     post '/sign_up', to: 'users#create', as: 'sign_up'
     get '/profile/:id', to: 'users#show', as: 'profile'
-  end
 end
